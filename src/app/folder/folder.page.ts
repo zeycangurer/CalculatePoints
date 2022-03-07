4a0a5398f12a72457911694ff069783a9437f4d3
Zeycan Gürer <63667132+zeycangurer@users.noreply.github.com>
2022-03-07 03:06:52 +0300

    first commit

---
+++ src/app/folder/folder.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-folder',
+  templateUrl: './folder.page.html',
+  styleUrls: ['./folder.page.scss'],
+})
+export class FolderPage implements OnInit {
+  public folder: string;
+  public mat:number = 0;
+  public fen:number = 0;
+  public tur:number = 0;
+  public sos:number = 0;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+}
